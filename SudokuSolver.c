e943c41575e927e3f7b48b1ac760396a93adaa64
Arthur <84559827+arthurdepina@users.noreply.github.com>
2023-04-15 13:15:14 -0300

    Solver per se
    I'm pretty sure that the solving function is now working. Still have to test it once the "verifica_possibilidade" function is done.

--- SudokuSolver.c
+++ SudokuSolver.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 void mostrar_tabuleiro(int **m, int linhas, int colunas){
     int i, j, k;
@@ -27,8 +28,31 @@ int alocando_sudoku(int **tabuleiro, int linhas, int colunas){
     return 0;
 }
 
-int resolve_sudoku(int **tabuleiro, int linhas, int colunas){
-    return 0;
+bool verifica_possibilidade(int **tabuleiro, int n, int x, int y){
+    // A função vai verificar se é possível inserir um número n nas coordenadas (x, y) de um tabuleiro de sudoku
+    
+    return true;
+}
+
+int resolve_sudoku(int **tabuleiro){
+    int n;
+    for(int l = 0; l < 9; l++){
+        for(int c = 0; c < 9; c++){
+            if (tabuleiro[l][c] == 0){
+                for(int n = 1; n <= 9; n++){
+                    if(verifica_possibilidade(tabuleiro, n, l, c)){
+                        tabuleiro[l][c] = n;
+                        if(resolve_sudoku(tabuleiro)){
+                            return 1; // Solucionado
+                        }
+                    tabuleiro[l][c] = 0;
+                    }
+                }
+            return 0; // Sem solução
+            }
+        }
+    }
+    return 1; // Resolvido
 }
 
 
@@ -52,7 +76,7 @@ int main(){
     mostrar_tabuleiro(tabuleiro, linhas, colunas);
     
     printf("\nTabuleiro Resolvido:\n\n");
-    // resolve_sudoku(tabuleiro, linhas, colunas);
+    // resolve_sudoku(tabuleiro);
 
     for(int i = 0; i < linhas; i++){
         free(tabuleiro[i]);
